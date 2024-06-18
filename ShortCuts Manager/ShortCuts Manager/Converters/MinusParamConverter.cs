348db0f14d70e4ebae8995004bf79fafa2d2e92d
Kamil Dyś <kamildys79@gmail.com>
2024-06-18 15:13:17 +0200

    x

---
+++ ShortCuts Manager/ShortCuts Manager/Converters/MinusParamConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ShortCuts_Manager.Converters
+{
+    public class MinusParamConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double originalWidth && parameter is string paramString && double.TryParse(paramString, out double param))
+            {
+                return originalWidth - param;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
